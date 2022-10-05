837b781e8823d5953664c2f09d5a458079968aee
Buddhika Gunathilaka <buddhika162@gmail.com>
2022-10-05 12:43:02 +0530

    first commit with one repository and unit tests

---
+++ src/Entity/Server.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ServerRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ServerRepository::class)]
+class Server
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $assetId = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $brand = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?int $price = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAssetId(): ?int
+    {
+        return $this->assetId;
+    }
+
+    public function setAssetId(int $assetId): self
+    {
+        $this->assetId = $assetId;
+
+        return $this;
+    }
+
+    public function getBrand(): ?string
+    {
+        return $this->brand;
+    }
+
+    public function setBrand(string $brand): self
+    {
+        $this->brand = $brand;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+}
---
+++ tests/ServerTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Tests;
+
+use App\Entity\Server;
+use App\Repository\ServerRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class ServerTest extends KernelTestCase
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    private EntityManagerInterface $entityManager;
+    protected function setUp(): void
+    {
+        $kernel = self::bootKernel();
+        DatabasePrimer::prime($kernel);
+
+        $this->entityManager = $kernel->getContainer()->get("doctrine")->getManager();
+        $this->managerRegistery = $kernel->getContainer()->get("doctrine");
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        $this->entityManager->close();
+    }
+
+    /*
+     *
+     */
+    public function testGetAllServers()
+    {
+        $server = new Server();
+        $server->setAssetId(123456);
+        $server->setBrand("Dell");
+        $server->setName("R210");
+        $server->setPrice(500);
+
+        $serverRepository = new ServerRepository($this->managerRegistery);
+        $serverRepository->add($server);
+        $savedServer = $serverRepository->findOneBy(['name' => 'R210']);
+        $this->assertNotNull($savedServer->getId());
+        $this->assertEquals(123456,$savedServer->getAssetId());
+        $this->assertEquals("R210",$savedServer->getName());
+        $this->assertEquals("Dell",$savedServer->getBrand());
+        $this->assertEquals(500,$savedServer->getPrice());
+    }
+
+}
